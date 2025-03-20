2d46dc36c4f24db4f242595cd2ed760fb5c82284
vetPan <pankratovjob@ya.ru>
2025-03-20 17:26:05 +0300

    reCreate app project

---
+++ app/build.gradle.kts
@@ -0,0 +1,26 @@
+plugins {
+    id("java")
+    id("se.patrikerdes.use-latest-versions") version "0.2.18"
+    id("com.github.ben-manes.versions") version "0.41.0"
+    application
+}
+
+group = "hexlet.code"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.9.1"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+application {
+    mainClass.set("hexlet.code.App")
+}
