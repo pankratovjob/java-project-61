0db4374847c00ee41b28bf57dfed9aa120c77add
vetPan <pankratovjob@ya.ru>
2025-03-25 08:56:00 +0300

    bugFix build.yml

--- app/src/main/java/hexlet/code/games/Cli.java
+++ app/src/main/java/hexlet/code/games/Cli.java
@@ -1,8 +1,20 @@
 package hexlet.code.games;
 
 import hexlet.code.Engine;
+import java.util.Map;
+
 public class Cli {
     public static String askNamePlayer() {
+
+
+        Map<String, String> env = System.getenv();
+
+        for (String envName : env.keySet()) {
+            System.out.format("%s=%s%n", envName,
+                    env.get(envName));
+        }
+
+
         return Engine.askNamePlayer();
     }
 }
